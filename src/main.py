e710e8b7398c0e7cc2d16b2f45c4a372fa1e9954
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-12-26 11:39:15 +0200

    added the ability to view logs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 
 from service_vsearch import search4letters
 
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> 'None':
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -12,6 +17,7 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
@@ -25,5 +31,17 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 
-app.run(debug=True)
+@app.route('/viewlog')
+def view_the_log() -> 'str':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    return str(contents)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
