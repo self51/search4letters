b28220ebba1b3478f3bb227fdbebdf7b60e8c954
self <ggdiscone@gmail.com>
2023-01-08 23:30:09 +0200

    added login, logout and logged in checker

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from service_vsearch import search4letters
 
 from DBcm import UseDatabase
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -9,6 +10,18 @@ app.config['dbconfig'] = dbconfig = {'host': '127.0.0.1',
                                      'user': 'vsearch',
                                      'password': 'vsearchpasswd',
                                      'database': 'vsearchlogDB', }
+
+
+@app.route('/login')
+def do_login() -> 'str':
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+
+@app.route('/logout')
+def do_logout() -> 'str':
+    session.pop('logged_in')
+    return 'You are now logged out.'
 
 
 def log_request(req: 'flask_request', res: str) -> 'None':
@@ -47,6 +60,7 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
 
     with UseDatabase(app.config['dbconfig']) as cursor:
@@ -62,6 +76,8 @@ def view_the_log() -> 'html':
                            the_row_titles=titles,
                            the_data=contents,)
 
+
+app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 if __name__ == '__main__':
     app.run(debug=True)
