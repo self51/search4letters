7354efc3376ff004eea43fb6a36e43d30788788e
self <ggdiscone@gmail.com>
2023-01-17 00:57:19 +0200

    added ability to handle some errors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session
 from service_vsearch import search4letters
 
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialError, SQLError
 from checker import check_logged_in
 
 app = Flask(__name__)
@@ -45,7 +45,12 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('*****Logging failed with this error: ', str(err))
+
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
@@ -62,19 +67,28 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = '''select phrase, letters, ip, browser_string, results 
+                      from log '''
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
 
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = '''select phrase, letters, ip, browser_string, results 
-                  from log '''
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
 
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_titles=titles,
-                           the_data=contents,)
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error: ', str(err))
+    except CredentialError as err:
+        print('User-id/Password issues. Error: ', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error: ', str(err))
+    except Exception as err:
+        print('Something went wrong: ', str(err))
+    return 'Error'
 
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
